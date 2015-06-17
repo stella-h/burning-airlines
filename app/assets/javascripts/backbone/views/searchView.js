74e2522dd2bed02455e33fdcb005fd8f1db38e6d
Christopher Barnaby <cjbarnaby@gmail.com>
2015-06-17 16:45:26 +1000

    flight info added to flightView

--- app/assets/javascripts/backbone/views/searchView.js
+++ app/assets/javascripts/backbone/views/searchView.js
@@ -46,57 +46,61 @@ var findFlights = function(origin, destination, dateOne, dateTwo) {
 
   var result = [];
 
-  if (origin !== "" && destination !== "") {
+//Origin and destination entered (dates entered or empty).
+//Results displayed: Origin and destination match, dates either matched or empty.  
+  if (origin !== "" && destination !== "") { 
     for (var i = 0; i < app.allFlights.toJSON().length; i++) {
       var thisOri = app.allFlights.toJSON()[i].origin;
       var thisDest = app.allFlights.toJSON()[i].destination;
       var thisDate = Date.parse(app.allFlights.toJSON()[i].date);
 
       dateMatchFunc();
-      
-      if (thisOri === origin.toUpperCase() && thisDest === destination.toUpperCase()) {
-        console.log('the date, destination and origin match');
+
+      if (thisOri === origin.toUpperCase() && thisDest === destination.toUpperCase() && dateMatch) {
         result.push(app.allFlights.models[i]);
       }
     }
-  } else if (destination !== "") {
+//Destination entered, origin not entered (dates entered or empty).
+//Results displayed: Destination matches, dates either matched or empty. 
+  } else if (destination !== "") { 
     for (var j = 0; j < app.allFlights.toJSON().length; j++) {
       var thisDest = app.allFlights.toJSON()[j].destination;
       var thisDate = Date.parse(app.allFlights.toJSON()[j].date);
       dateMatchFunc();
+
       if (thisDest === destination.toUpperCase() && dateMatch) {
-        console.log('the destination and date is a match!');
         result.push(app.allFlights.models[j]);
       }
     }
-  } else if (origin !== "") {
+//Origin entered, destination not entered (dates entered or empty).
+//Results displayed: Origin matches, dates either matched or empty. 
+  } else if (origin !== "") { 
     for (var k = 0; k < app.allFlights.toJSON().length; k++) {
       var thisOri = app.allFlights.toJSON()[k].origin;
       var thisDate = Date.parse(app.allFlights.toJSON()[k].date);
       dateMatchFunc();
       if (thisOri === origin.toUpperCase() && dateMatch) {
-        console.log('the origin and date are a match!');
         result.push(app.allFlights.models[k]);
       }
     }
-  } else {
+  } else { 
+//Neither origin nor destination entered. 
+//Results displayed: If dates empty, returns all flights. Otherwise returns flights with matching dates. 
+
     for (var k = 0; k < app.allFlights.toJSON().length; k++) {
       var thisDate = Date.parse(app.allFlights.toJSON()[k].date);
       dateMatchFunc();
       if (dateMatch) {
-        console.log('this should juust be all flights');
         result.push(app.allFlights.models[k]);
       }
     }
   }
-  console.log("this is the result", result);
   renderResults(result);
 };
 
 var renderResults = function(results) {
   $('#searchResults').html('');
      if (results.length === 0) {
-      console.log("here")
     $('#searchResults').html('<p>No flights match your search</p>')
   } else {
     for ( var i = 0; i < results.length; i++ ) {
