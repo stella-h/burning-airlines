7d4395f78ee743287d9a7225020392055655ade9
Christopher Barnaby <cjbarnaby@gmail.com>
2015-06-17 16:46:58 +1000

    I don't even know anymore

--- app/assets/javascripts/backbone/views/flightView.js
+++ app/assets/javascripts/backbone/views/flightView.js
@@ -10,6 +10,7 @@ app.FlightView = Backbone.View.extend({
     var data = this.model.toJSON();
     data.rows = rows;
     data.columns = columns;
+    data.planeName = app.allPlanes.toJSON()[data.plane_id-1].name;  //Michael wrote this code. Blame him.
 
     var toAppend = this.$el.html(flightHTML(data));
 
